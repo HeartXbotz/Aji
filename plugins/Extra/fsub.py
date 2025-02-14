84a8ea237b2ea46792f9a4da2564610002ef832e
HeartXbotz <heartthief0801@gmail.com>
2025-02-14 12:52:39 +0530

    Update fsub.py

--- plugins/Extra/fsub.py
+++ plugins/Extra/fsub.py
@@ -7,32 +7,45 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-@Client.on_message(filters.command("fsub"))
-async def force_subscribe(client, message):
-    m = await message.reply_text("Wait im checking...")
-    if not message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        return await m.edit("This command is only for groups!")
-    if not await is_check_admin(client, message.chat.id, message.from_user.id):
-        return await m.edit("Only group admins can use this command!")
-    try: 
-        toFsub = message.command[1]
-    except IndexError:
-        return await m.edit("Usage: /fsub CHAT_ID")
-    if not toFsub.startswith("-100"):
-        toFsub = '-100'+toFsub
-    if not toFsub[1:].isdigit() or len(toFsub) != 14:
-        return await m.edit("CHAT_ID isn't valid!")
-    toFsub = int(toFsub)
-    if toFsub == message.chat.id:
-        return await m.edit("It seems like you're attempting to enable force subscription for this chat ID. Please use a different chat ID !")
-    if not await is_check_admin(client, toFsub, client.me.id):
-        return await m.edit("I need to be an admin in the given chat to perform this action!\nMake me admin in your Target chat and try again.")
+@Client.on_message(filters.command('fsub'))
+async def fsub(client, message):
+    userid = message.from_user.id if message.from_user else None
+    if not userid:
+        return await message.reply(f"<b>You are anonymous admin. Turn off anonymous admin and try again this command</b>")
+    chat_type = message.chat.type
+    if chat_type == enums.ChatType.PRIVATE:
+        return await message.reply_text("<b>This Command Work Only in group\n\nTry it in your own group</b>")
+    elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+        grpid = message.chat.id
+        title = message.chat.title
+    else:
+        return
+    userid = message.from_user.id
+    user = await client.get_chat_member(grpid, userid)
+    if user.status != enums.ChatMemberStatus.ADMINISTRATOR and user.status != enums.ChatMemberStatus.OWNER and str(userid) not in ADMINS:
+        return
+    else:
+        pass
     try:
-        await db.setFsub(grpID=message.chat.id, fsubID=toFsub)
-        return await m.edit(f"Successfully added force subscribe to {toFsub} in {message.chat.title}")
-    except Exception as e:
-        logger.exception(e)
-        return await m.edit(f"Something went wrong ! Try again later or report in Support Group @Jisshu_support")
+        ids = message.text.split(" ", 1)[1]
+        fsub_ids = [int(id) for id in ids.split()]
+    except IndexError:
+        return await message.reply_text("<b>Command Incomplete!\n\nAdd Multiple Channel By Seperate Space. Like: /fsub id1 id2 id3</b>")
+    except ValueError:
+        return await message.reply_text('<b>Make Sure Ids are Integer.</b>')        
+    channels = "Channels:\n"
+    for id in fsub_ids:
+        try:
+            chat = await client.get_chat(id)
+        except Exception as e:
+            return await message.reply_text(f"<b>{id} is invalid!\nMake sure this bot admin in that channel.\n\nError - {e}</b>")
+        if chat.type != enums.ChatType.CHANNEL:
+            return await message.reply_text(f"<b>{id} is not channel.</b>")
+        channels += f'{chat.title}\n'
+    await save_group_settings(grpid, 'fsub', fsub_ids)
+    await message.reply_text(f"<b>Successfully set force channels for {title} to\n\n{channels}\n\nYou can remove it by /nofsub.</b>")
+            
+    
 
 @Client.on_message(filters.command("del_fsub"))
 async def del_force_subscribe(client, message):
